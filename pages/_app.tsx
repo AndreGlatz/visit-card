9bc7270c6cfcde6b784bb7627660e931a66e05e2
André Glatz <andreglatz@outlook.com>
2020-09-19 16:59:10 -0300

    add styled-components global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import GlobalStyles from "@App/styles/globals";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <GlobalStyles />
+    </>
+  );
 }
 
 export default MyApp;
